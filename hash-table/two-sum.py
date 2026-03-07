f778f770d5091785d2da514efb275ed05e435c37
boyazhang54 <boyazhang54@gmail.com>
2026-03-07 17:44:20 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -5,9 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        comp_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in comp_map:
-                return [comp_map[complement], i]
-            comp_map[num] = i
+        for i in nums:
+            for j in nums:
+                if i + j = target:
+                    return [i, j]
