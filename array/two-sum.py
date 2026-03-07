238a24d67c085478990a675eeda961d6eafcb47a
boyazhang54 <boyazhang54@gmail.com>
2026-03-07 17:38:12 +0800

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,5 +1,10 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         comp_map = {}
         for i, num in enumerate(nums):
             complement = target - num
