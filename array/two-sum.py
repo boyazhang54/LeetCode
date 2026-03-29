e84cfd8c2dd463b0cc4b5d5a2f13bbbfed7dae3b
boyazhang54 <boyazhang54@gmail.com>
2026-03-29 19:02:32 +0800

    LC-two-sum [Runtime: 0 ms; Memory: 12.9 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -5,7 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1,(len(nums))):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] =  i
